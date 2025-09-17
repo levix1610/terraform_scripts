96d55879cbe78db9afb96efc048f280e219f1ac8
levix1610 <andy4991@gmail.com>
2025-09-17 07:01:59 -0400

    add .tf example

---
+++ main-example.tf
@@ -0,0 +1,40 @@
+# Example of a .tf config
+
+terraform {
+    required_providers {
+        proxmox = {
+            source = "telmate/proxmox"
+        }
+    }
+}
+
+provider "proxmox" {
+    pm_api_url          = "https://url-to-proxmox-server:8006/api2/json"
+    pm_api_token_id     = "your-token-id"
+    pm_api_token_secret = "your-secret"
+    pm_tls_insecure     = true
+}
+
+resource "proxmox_vm_qemu" "vm-instance" {
+    name                = "vm-instance"
+    target_node         = "pve-1"
+    clone               = "your-template-name"
+    full_clone          = true
+    cores               = 2
+    memory              = 2048
+
+    disk {
+        size            = "32G"
+        type            = "scsi"
+        storage         = "your-storage-volume"
+        discard         = "on"
+    }
+
+    network {
+        model     = "virtio"
+        bridge    = "vmbr1"
+        firewall  = false
+        link_down = false
+    }
+
+}
