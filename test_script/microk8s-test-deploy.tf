164d7777fe4c60c7c44a827c8cea1e8cd3c053a9
levix1610 <andy4991@gmail.com>
2025-09-19 23:15:45 -0400

    Change test script to new temple for testing.

--- test_script/microk8s-test-deploy.tf
+++ test_script/microk8s-test-deploy.tf
@@ -27,7 +27,7 @@ resource "proxmox_virtual_environment_vm" "vmus-test-k8s-01" {
     name                = "vmus-test-k8s-01"
     
     clone {
-        vm_id           = "110" # VM ID of the template.
+        vm_id           = "101" # VM ID of the template. Trying with a modified version.
         full            = true
     }
     
