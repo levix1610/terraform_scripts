be445fb5a2bc983cdb4e2d363a8d640faa1496aa
levix1610 <andy4991@gmail.com>
2025-09-20 22:21:59 -0400

    Fix outputs for loop.

--- test_cluster/outputs.tf
+++ test_cluster/outputs.tf
@@ -6,7 +6,7 @@ output "all_vm_details" {
   description = "Map of all VMs with their IP address and hostname."
   value = {
     "vmus-test-k8s-01" = {
-      ip       = proxmox_virtual_environment_vm.vmus-test-k8s-01.ipv4_address
+      ip       = proxmox_virtual_environment_vm.vmus-test-k8s-01.ipv4_addresses[0]
       hostname = proxmox_virtual_environment_vm.vmus-test-k8s-01.name
     }
   }
