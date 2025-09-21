a47865a46d06271fc192cbd315a06ebb72416bae
levix1610 <andy4991@gmail.com>
2025-09-21 01:25:15 -0400

    Change to for-loop. agent issue.

--- test_cluster/outputs.tf
+++ test_cluster/outputs.tf
@@ -1,12 +1,13 @@
 # Data output after all scripts have run.
 
 
-output "vm_hostnames" {
-  description = "The hostnames of the Kubernetes nodes."
-  value       = proxmox_virtual_environment_vm.vmus-test-k8s-01.name
-}
 
-output "vm_ip_addresses" {
-  description = "The IP addresses of the Kubernetes nodes."
-  value       = proxmox_virtual_environment_vm.vmus-test-k8s-01.ipv4_addresses
+output "all_vm_details" {
+  description = "Map of all VMs with their IP address and hostname."
+  value = {
+    "vmus-test-k8s-01" = {
+      ip       = proxmox_virtual_environment_vm.vmus-test-k8s-01.ipv4_addresses[0]
+      hostname = proxmox_virtual_environment_vm.vmus-test-k8s-01.name
+    }
+  }
 }
