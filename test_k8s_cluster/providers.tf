dc4b63fe7077921c84de043a699b5dec77771047
levix1610 <andy4991@gmail.com>
2025-10-09 00:07:54 -0400

    remove vault - moving secrets to actions.

--- test_k8s_cluster/providers.tf
+++ test_k8s_cluster/providers.tf
@@ -10,16 +10,6 @@ terraform {
     }
 }
 
-provider "vault" {
-  address = "https://10.0.100.80:8200"
-  token   = var.vault_token
-  
-}
-
-data "vault_generic_secret" "database" {
-  path = "cubbyhole/secret/terraform"
-}
-
 # Connection/communication to the server
 provider "proxmox" {
     endpoint            = "https://10.0.250.70:8006/"
