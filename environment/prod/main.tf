952d6d86deef2a97a579b0984908fc27e3dca98a
levix1610 <andy4991@gmail.com>
2025-10-14 11:00:45 -0400

    fix api variable.

--- environment/prod/main.tf
+++ environment/prod/main.tf
@@ -51,7 +51,7 @@ provider "proxmox" {
     
     # API Credentials (passing the values received from TF_VAR_... env vars):
     api_token_id        = var.prod.api_token_id
-    api_token_secret    = var.api_token_secret
+    api_token_secret    = var.prod.api_token_secret
     
     # ... include all other variables required by the microk8s_cluster module
 }
