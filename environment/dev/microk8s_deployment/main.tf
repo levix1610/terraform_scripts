eec8c2cd7f0cd82425e9687d0f1fccf38eb8417f
levix1610 <andy4991@gmail.com>
2026-02-16 01:49:49 -0500

    fix api variable in mains for dev

--- environment/dev/microk8s_deployment/main.tf
+++ environment/dev/microk8s_deployment/main.tf
@@ -15,7 +15,7 @@ terraform {
 # Connection/communication to the server
 provider "proxmox" {
     endpoint            = "https://10.0.250.50:8006/"
-    api_token           = "${var.prod_api_token_id}=${var.prod_api_token_secret}"
+    api_token           = "${var.test_api_token_id}=${var.test_api_token_secret}"
     insecure            = true
 }
 
