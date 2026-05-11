8e7b6d096870f7242e6096bfcc21ab6a137ad4bf
levix1610 <andy4991@gmail.com>
2026-05-11 02:50:40 -0400

    fix api to prod

--- environment/prod/portainer/main.tf
+++ environment/prod/portainer/main.tf
@@ -14,7 +14,7 @@ terraform {
 # Connection/communication to the server
 provider "proxmox" {
     endpoint            = "https://10.0.250.50:8006/"
-    api_token           = "${var.test_api_token_id}=${var.test_api_token_secret}"
+    api_token           = "${var.prod_api_token_id}=${var.prod_api_token_secret}"
     insecure            = true
 }
 
@@ -49,8 +49,8 @@ provider "proxmox" {
     memory              = var.memory
     
     # API Credentials (passing the values received from TF_VAR_... env vars):
-    api_token_id        = var.test_api_token_id
-    api_token_secret    = var.test_api_token_secret
+    api_token_id        = var.prod_api_token_id
+    api_token_secret    = var.prod_api_token_secret
     
     # ... include all other variables required by the microk8s_cluster module
 }
